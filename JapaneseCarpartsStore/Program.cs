b5e9ee568a2e1358f8f8609d045409808207f394
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-01-31 22:40:13 +0200

    Configured connection string and registered the DbContext in Program.cs

--- JapaneseCarpartsStore/Program.cs
+++ JapaneseCarpartsStore/Program.cs
@@ -1,3 +1,6 @@
+using JapaneseCarpartsStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace JapaneseCarpartsStore
 {
     public class Program
@@ -5,6 +8,12 @@ namespace JapaneseCarpartsStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddControllersWithViews();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -15,7 +24,6 @@ namespace JapaneseCarpartsStore
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
