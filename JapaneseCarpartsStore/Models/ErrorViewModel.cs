27cd211185170146f994f1dc5ba96f8ae763f6fe
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-01-31 21:20:03 +0200

    Initial project setup

---
+++ JapaneseCarpartsStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JapaneseCarpartsStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
