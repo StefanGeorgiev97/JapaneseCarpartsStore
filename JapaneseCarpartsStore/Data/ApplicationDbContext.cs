756b038502b502d96ec2eb16b4734337862769fe
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-01-31 22:32:03 +0200

    Installed EF Core packages and created ApplicationDbContext

---
+++ JapaneseCarpartsStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using JapaneseCarpartsStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JapaneseCarpartsStore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<VehicleModel> VehicleModels { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
+            modelBuilder.Entity<Part>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
