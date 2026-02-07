76bf6b4b0c4f83b95ed8dc8400d1fa29649747e6
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-02-08 00:03:13 +0200

    Implemented part creation functionality in Admin area

--- JapaneseCarpartsStore/Controllers/AdminController.cs
+++ JapaneseCarpartsStore/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using JapaneseCarpartsStore.Data;
+using JapaneseCarpartsStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,7 @@ namespace JapaneseCarpartsStore.Controllers
             _context = context;
         }
 
-        //Get Admin/Index
+        //GET: Admin/Index
         public async Task<IActionResult> Index()
         {
             //We include VehicleModel and Brand so we can show the brand and model next to the part name
@@ -23,6 +24,45 @@ namespace JapaneseCarpartsStore.Controllers
                 .ToListAsync();
 
             return View(parts);
+        }
+        //GET: Admin/Create
+        public IActionResult Create()
+        {
+            //We need a list of Vehicle Models for the dropdown
+            //Format the text to show "Honda Civic (2016-2021)"
+            ViewData["VehicleModelId"] = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(
+                _context.VehicleModels.Include(m => m.Brand)
+                .Select(m => new {
+                    Id = m.Id,
+                    DisplayText = $"{m.Brand.Name} {m.Name} ({m.YearStart}-{m.YearEnd})"
+                }),
+                "Id", "DisplayText");
+
+            return View();
+        }
+
+        //POST: Admin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,ImageUrl,Category,VehicleModelId")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(part);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            //If validation fails, reload the dropdown so the form doesn't break
+            ViewData["VehicleModelId"] = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(
+                _context.VehicleModels.Include(m => m.Brand)
+                .Select(m => new {
+                    Id = m.Id,
+                    DisplayText = $"{m.Brand.Name} {m.Name} ({m.YearStart}-{m.YearEnd})"
+                }),
+                "Id", "DisplayText", part.VehicleModelId);
+
+            return View(part);
         }
     }
 }
