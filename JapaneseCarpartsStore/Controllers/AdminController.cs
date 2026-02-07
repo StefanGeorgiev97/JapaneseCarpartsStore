26d0ff596213aefe78c79fbedd4f1eefc8077e5e
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-02-07 22:41:59 +0200

    Created the admin controller and index view, added the Administrator button to the navbar

---
+++ JapaneseCarpartsStore/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using JapaneseCarpartsStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JapaneseCarpartsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get Admin/Index
+        public async Task<IActionResult> Index()
+        {
+            //We include VehicleModel and Brand so we can show the brand and model next to the part name
+            var parts = await _context.Parts
+                .Include(p => p.VehicleModel)
+                .ThenInclude(vm => vm.Brand)
+                .ToListAsync();
+
+            return View(parts);
+        }
+    }
+}
