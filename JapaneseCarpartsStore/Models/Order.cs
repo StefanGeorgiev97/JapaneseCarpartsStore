f7b331e0e833a21d241c763fa83a6de500737e92
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-04-05 18:55:09 +0300

    Implemented Order and OrderItem entities for checkout system

---
+++ JapaneseCarpartsStore/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JapaneseCarpartsStore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalPrice { get; set; }
+        public string UserId { get; set; } = null!;
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ JapaneseCarpartsStore/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JapaneseCarpartsStore.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+        public int PartId { get; set; }
+        [ForeignKey(nameof(PartId))]
+        public Part Part { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal PriceAtPurchase { get; set; }
+    }
+}
