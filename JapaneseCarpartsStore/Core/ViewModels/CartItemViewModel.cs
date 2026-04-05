7d0414550fe9ee1ba338e17842fece75b676a672
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-04-05 20:36:59 +0300

    Added CartItemViewModel for decoupled session storage

---
+++ JapaneseCarpartsStore/Core/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JapaneseCarpartsStore.Core.ViewModels
+{
+    public class CartItemViewModel
+    {
+        public int PartId { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
