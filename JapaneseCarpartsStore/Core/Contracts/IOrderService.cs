3e6856e02bbf6e995e2fb1ba3174fdbd9656fd31
Stefan Georgiev <stefan.georgiev9722@gmail.com>
2026-04-05 19:13:27 +0300

    Defined IOrderService contract for checkout business logic

---
+++ JapaneseCarpartsStore/Core/Contracts/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using JapaneseCarpartsStore.Models;
+
+namespace JapaneseCarpartsStore.Core.Contracts
+{
+    public interface IOrderService
+    {
+        Task CreateOrderAsync(string userId, List<int> partIds);
+        Task<IEnumerable<Order>> GetUserOrdersAsync(string userId);
+    }
+}
